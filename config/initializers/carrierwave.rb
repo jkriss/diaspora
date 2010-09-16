8cd56c83bb349c7311f47112140539a2acee8135
Jesse Kriss <jesse@jklabs.net>
2010-09-15 18:47:57 -0700

    trying out s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,8 @@
 
 
 CarrierWave.configure do |config|
-  config.storage = :file
+  config.storage = :s3
+  config.s3_access_key_id = ENV['S3_ACCESS_KEY']
+  config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+  config.s3_bucket = ENV['S3_BUCKET'] || 'diaspora'
 end
