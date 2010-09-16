78eaeb17ffe3f419ec29764c94be633bb6b71b59
Jesse Kriss <jesse@jklabs.net>
2010-09-15 18:21:49 -0700

    trying again for heroku mongodb config

--- config/initializers/_mongo.rb
+++ config/initializers/_mongo.rb
@@ -6,9 +6,9 @@ if ENV['MONGOHQ_URL']
   MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
 else
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "diaspora-#{Rails.env}"
 end
 
-MongoMapper.database = "diaspora-#{Rails.env}"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
